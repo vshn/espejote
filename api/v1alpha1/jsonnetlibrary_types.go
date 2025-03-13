39636f89fc9aaf33de3fbf12e374661c5e06b30e
Sebastian Widmer <sebastian.widmer@vshn.net>
2025-03-13 13:21:42 +0100

    Remove status subresource from JsonnetLibrary. Cleanup comments

--- api/v1alpha1/jsonnetlibrary_types.go
+++ api/v1alpha1/jsonnetlibrary_types.go
@@ -4,30 +4,21 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // JsonnetLibrarySpec defines the desired state of JsonnetLibrary.
 type JsonnetLibrarySpec struct {
+	// Data is a map of Jsonnet library files.
+	// The key is the file name and the value is the file content.
 	Data map[string]string `json:"data,omitempty"`
 }
 
-// JsonnetLibraryStatus defines the observed state of JsonnetLibrary.
-type JsonnetLibraryStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
 // +kubebuilder:object:root=true
-// +kubebuilder:subresource:status
 
 // JsonnetLibrary is the Schema for the jsonnetlibraries API.
 type JsonnetLibrary struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   JsonnetLibrarySpec   `json:"spec,omitempty"`
-	Status JsonnetLibraryStatus `json:"status,omitempty"`
+	Spec JsonnetLibrarySpec `json:"spec,omitempty"`
 }
 
 // +kubebuilder:object:root=true
