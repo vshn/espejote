e5c6bf85426c2b7bf43e76f312e88a6897689588
Sebastian Widmer <sebastian.widmer@vshn.net>
2025-03-28 09:23:24 +0100

    cleanup more copy-paste errors

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,4 +1,4 @@
-// Package v1alpha1 contains API Schema definitions for the managedupgrade v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the espejote.io v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=espejote.io
 package v1alpha1
