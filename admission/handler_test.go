c853ee15f32f89376d6f79ffdd4334d6d7e4519a
Sebastian Widmer <sebastian.widmer@vshn.net>
2025-03-31 10:33:42 +0200

    Fix crash when using `admission.assertPatch` with integers
    Current go-jsonnet only supports `float64` as NativeFunction return values. See
    <https://github.com/google/go-jsonnet/issues/761> and <https://github.com/google/go-jsonnet/pull/753>.
    
    This PR ensures the `__internal_use_espejote_lib_function_apply_json_patch` function only returns `float64` by switching the kubernetes json unmarshaler to the `encoding/json` unmarshaler.
    
    As we unmarshal into `any` we don't need any special handling from the kubernetes unmarshaller.

--- admission/handler_test.go
+++ admission/handler_test.go
@@ -12,11 +12,13 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	admissionv1 "k8s.io/api/admission/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	authenticationv1 "k8s.io/api/authentication/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -158,16 +160,19 @@ func Test_Handler_Patched(t *testing.T) {
 		UserInfo: authenticationv1.UserInfo{
 			Username: "testuser",
 		},
-		Object: objectToRaw(t, &corev1.Namespace{
+		Object: objectToRaw(t, &appsv1.Deployment{
 			TypeMeta: metav1.TypeMeta{
-				APIVersion: "v1",
-				Kind:       "Namespace",
+				APIVersion: "apps/v1",
+				Kind:       "Deployment",
 			},
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test",
 				Annotations: map[string]string{
-					"blub": "blab",
+					"test": "test",
 				},
+			},
+			Spec: appsv1.DeploymentSpec{
+				Replicas: ptr.To(int32(3)),
 			},
 		}),
 	})
